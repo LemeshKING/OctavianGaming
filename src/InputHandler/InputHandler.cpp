04c386f1cbe87939970947d064c980d7f13aa198
lemeshking <lemeshkinggg@gmail.com>
2025-03-16 18:21:32 +0700

    +DefinitionClassInputHandler

---
+++ src/InputHandler/InputHandler.cpp
@@ -0,0 +1,7 @@
+#include "InputHandler.h"
+
+
+bool InputHandler::isButtonPresset()
+{
+   return false;
+}
---
+++ src/InputHandler/InputHandler.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "../Button/Button.h"
+
+class InputHandler
+{
+   bool isButtonPresset(/*std::unique_ptr<Button> &button, const sf::Vector2i &mousePos */);
+};
