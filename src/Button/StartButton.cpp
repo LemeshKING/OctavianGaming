aa4072e0b87071dcbc524cb9351cc1671e80589f
lemeshking <lemeshkinggg@gmail.com>
2025-03-17 00:13:35 +0700

    upd Button
    добавлен контур для кнопок

--- src/Button/StartButton.cpp
+++ src/Button/StartButton.cpp
@@ -5,6 +5,8 @@ StarButton::StarButton()
    buttonSprite.setPosition(sf::VideoMode::getDesktopMode().width * 3 / 4, sf::VideoMode::getDesktopMode().height / 5);
    buttonSprite.setSize(sf::Vector2f(sf::VideoMode::getDesktopMode().width / 5, sf::VideoMode::getDesktopMode().height / 6));
    buttonSprite.setFillColor(sf::Color::Green);
+   buttonSprite.setOutlineThickness(3);
+   buttonSprite.setOutlineColor(sf::Color::Black);
 }
 
 void StarButton::onButtonClick()
--- src/Button/StopButton.cpp
+++ src/Button/StopButton.cpp
@@ -5,6 +5,8 @@ StopButton::StopButton()
    buttonSprite.setPosition(sf::VideoMode::getDesktopMode().width * 3 / 4, sf::VideoMode::getDesktopMode().height * 2.75 / 4);
    buttonSprite.setSize(sf::Vector2f(sf::VideoMode::getDesktopMode().width / 5, sf::VideoMode::getDesktopMode().height / 6));
    buttonSprite.setFillColor(sf::Color::Red);
+   buttonSprite.setOutlineThickness(3);
+   buttonSprite.setOutlineColor(sf::Color::Black);
 }
 
 void StopButton::onButtonClick()
