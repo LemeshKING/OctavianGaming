69a50653d38ccedfdf4a8aaeb514c7038c9b3f5d
lemeshking <lemeshkinggg@gmail.com>
2025-03-18 13:22:09 +0700

    upd Game
    Добавлены условия переключения состояния в основном цикле

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -5,6 +5,7 @@ Game::Game()
 // инициализируем начально состояние, создаем кнопки.
    starButton = std::make_unique<StarButton>();
    stopButton = std::make_unique<StopButton>();
+   stateMachine.changeState(idleState);
 }
 
 void Game::run()
@@ -17,7 +18,22 @@ void Game::run()
       {
          if (event.type == sf::Event::Closed)
             window.close();
+         if (event.type == sf::Event::MouseButtonPressed) 
+            if (event.mouseButton.button == sf::Mouse::Left) 
+               if(inputHandler.isButtonPresset(starButton, sf::Mouse::getPosition(window)) && stateMachine.getState() == idleState)
+               {
+                  starButton->onButtonClick();
+                  stateMachine.changeState(spinningState);
+               }
+               else if(inputHandler.isButtonPresset(stopButton, sf::Mouse::getPosition(window)) && stateMachine.getState() == spinningState)
+               {
+                  stopButton->onButtonClick();
+                  stateMachine.changeState(resultState);
+               }
+            
+         
       }
+      stateMachine.update();
       window.clear(sf::Color::White);
       render.drawReels(slotMachine);
       render.drawButton(starButton);
--- src/Game/Game.h
+++ src/Game/Game.h
@@ -4,6 +4,9 @@
 #include "../SlotMachine/SlotMachine.h"
 #include "../StateMachine/StateMachine.h"
 #include "../InputHandler/InputHandler.h"
+#include "../StateMachine/IdleState.h"
+#include "../StateMachine/SpinningState.h"
+#include "../StateMachine/ResultState.h"
 
 typedef std::unique_ptr<Button> UPtrButton;
 
@@ -17,6 +20,9 @@ private:
       InputHandler inputHandler;
       UPtrButton starButton;
       UPtrButton stopButton;
+      SPtrState idleState = std::make_shared<IdleState>();
+      SPtrState spinningState = std::make_shared<SpinningState>(slotMachine);
+      SPtrState resultState = std::make_shared<ResultState>(slotMachine);
 
    
 public:
