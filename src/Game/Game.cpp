2dd15ba0460e6f8390924d808af9671976128eb9
lemeshking <lemeshkinggg@gmail.com>
2025-03-21 17:23:00 +0700

    upd Game
    Обновлен основной цикл игры

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -5,37 +5,68 @@ Game::Game()
 // инициализируем начально состояние, создаем кнопки.
    starButton = std::make_unique<StarButton>();
    stopButton = std::make_unique<StopButton>();
+   std::shared_ptr<ResultState> tmp1 = std::make_shared<ResultState>(*slotMachine);
+   tmp1->Attach(tmp);
+   resultState = tmp1;
    stateMachine.changeState(idleState);
+   
+
 }
 
 void Game::run()
 {
+
+   sf::Clock clock;  
+   sf::Clock clock1;  
+   float time;
+   float time2;
    sf::RenderWindow& window = render.getWindow();
    while (window.isOpen()) 
    {
+      if(stateMachine.getState() == idleState)
+         clock.restart();
+      time = clock.getElapsedTime().asSeconds();
+      if(stateMachine.getState() != spinningState)
+         clock1.restart();
+      time2 = clock1.getElapsedTime().asSeconds();
       sf::Event event;
       while (window.pollEvent(event)) 
       {
+      
          if (event.type == sf::Event::Closed)
             window.close();
+
+      }
+
          if (event.type == sf::Event::MouseButtonPressed) 
             if (event.mouseButton.button == sf::Mouse::Left) 
                if(inputHandler.isButtonPresset(starButton, sf::Mouse::getPosition(window)) && stateMachine.getState() == idleState)
                {
+                  countMatches = 0;
                   starButton->onButtonClick();
                   stateMachine.changeState(spinningState);
                }
-               else if(inputHandler.isButtonPresset(stopButton, sf::Mouse::getPosition(window)) && stateMachine.getState() == spinningState)
+               else if(inputHandler.isButtonPresset(stopButton, sf::Mouse::getPosition(window)) && stateMachine.getState() == spinningState && time > 1)
                {
                   stopButton->onButtonClick();
                   stateMachine.changeState(resultState);
+                  clock.restart();
+                  time = 0;
                }
             
          
+      
+      if (time2 > 3)
+      {
+         time2 = 0;
+         stateMachine.changeState(resultState);
       }
       stateMachine.update();
+
       window.clear(sf::Color::White);
-      render.drawReels(slotMachine);
+      if(stateMachine.getState() != spinningState)
+         render.drawResult(countMatches);
+      render.drawReels(*slotMachine);
       render.drawButton(starButton);
       render.drawButton(stopButton);
 
@@ -43,3 +74,24 @@ void Game::run()
       window.display();
    }
 }
+
+
+
+void Game::resultOver()
+{
+   stateMachine.changeState(idleState);
+}
+
+ResultStateObserver::ResultStateObserver(StateMachine& stateMachine, SPtrState &idleState)
+{
+   _stateMachine = &stateMachine;
+   _idleState = idleState;
+}
+
+void ResultStateObserver::resultOver(int& count)
+{
+   _stateMachine->changeState(_idleState);
+   Game::countMatches = count;
+}
+
+int Game::countMatches = 0;
--- src/Game/Game.h
+++ src/Game/Game.h
@@ -7,26 +7,44 @@
 #include "../StateMachine/IdleState.h"
 #include "../StateMachine/SpinningState.h"
 #include "../StateMachine/ResultState.h"
+#include "../StateMachine/IObserver.h"
+#include "../TextureManager/TextureManager.h"
+
 
 typedef std::unique_ptr<Button> UPtrButton;
 
 
-class Game
+
+class ResultStateObserver : public IObserver
+{
+   SPtrState _idleState;
+   StateMachine* _stateMachine;
+public:
+   ResultStateObserver(StateMachine& stateMachine, SPtrState &idleState);
+   void resultOver(int &count) override;
+};
+
+class Game 
 {
 private:
+      TextureManager textureManager;
       StateMachine stateMachine;
-      SlotMachine slotMachine;
+      SlotMachine *slotMachine = new SlotMachine(textureManager);
       Render render;
       InputHandler inputHandler;
       UPtrButton starButton;
       UPtrButton stopButton;
       SPtrState idleState = std::make_shared<IdleState>();
-      SPtrState spinningState = std::make_shared<SpinningState>(slotMachine);
-      SPtrState resultState = std::make_shared<ResultState>(slotMachine);
+      SPtrState spinningState = std::make_shared<SpinningState>(*slotMachine);
+      SPtrState resultState = std::make_shared<ResultState>(*slotMachine);
+      SPtrObserver tmp = std::make_shared<ResultStateObserver>(stateMachine, idleState);
 
    
 public:
    Game();
    void run();
+   void resultOver();
+   static int countMatches;
 };
+
 
