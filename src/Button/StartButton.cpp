0411d31d8a3ad3e4d71c0958c0c2c6776f4e7a92
lemeshking <lemeshkinggg@gmail.com>
2025-03-21 17:15:08 +0700

    +upd Button classes
    Добавлен текст для кнопок

--- src/Button/StartButton.cpp
+++ src/Button/StartButton.cpp
@@ -2,11 +2,20 @@
 
 StarButton::StarButton()
 {
+   font.loadFromFile("resources/ttt.otf");
+   text.setFont(font);
+   text.setOutlineThickness(0.5);
+   text.setFillColor(sf::Color::Black);
+   text.setString("Start");
+
    buttonSprite.setPosition(sf::VideoMode::getDesktopMode().width * 3 / 4, sf::VideoMode::getDesktopMode().height / 5);
    buttonSprite.setSize(sf::Vector2f(sf::VideoMode::getDesktopMode().width / 5, sf::VideoMode::getDesktopMode().height / 6));
    buttonSprite.setFillColor(sf::Color::Green);
    buttonSprite.setOutlineThickness(3);
    buttonSprite.setOutlineColor(sf::Color::Black);
+
+   text.setCharacterSize(buttonSprite.getSize().x / 3);
+   text.setPosition(buttonSprite.getPosition().x + buttonSprite.getSize().x / 8, buttonSprite.getPosition().y);
 }
 
 void StarButton::onButtonClick()
