7a6e1d77a4be35d1accca5b40ea26167ce0963a3
lemeshking <lemeshkinggg@gmail.com>
2025-03-21 17:16:37 +0700

    upd button, bug fix

--- src/Button/Button.cpp
+++ src/Button/Button.cpp
@@ -9,22 +9,16 @@ sf::RectangleShape Button::getSprite()
 {
     return buttonSprite;
 }
-<<<<<<< Updated upstream
-=======
-<<<<<<< Updated upstream
-=======
->>>>>>> Stashed changes
+
 
 sf::Vector2f Button::getSize()
 {
    return buttonSprite.getSize();
 }
-<<<<<<< Updated upstream
-=======
+
 
 sf::Text Button::getText()
 {
    return text;
 }
->>>>>>> Stashed changes
->>>>>>> Stashed changes
+
